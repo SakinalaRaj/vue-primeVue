6516575333aa06d35a634737006b85f5218b3dbc
SakinalaRaj <143786644+SakinalaRaj@users.noreply.github.com>
2023-10-11 17:55:00 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card flex justify-content-center">
+    <label for="text">text</label>
+    <InputText id="text" v-model="text" />
+    <label for="name">Name</label>
+    <InputText id="name" v-model="name" placeholder="enter here" />
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      name: "direct value",
+      text: ref("refValue"),
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import InputText from "primevue/inputtext";
+
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+
+const app = createApp(App);
+app.use(PrimeVue);
+app.component("InputText", InputText);
+app.mount("#app");
